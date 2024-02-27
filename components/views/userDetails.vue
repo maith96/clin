637e9d0226f635045709cb931a1df10b16bde342
maith96 <threedaniel385@gmail.com>
2024-02-27 09:24:55 +0300

    Improved styling and include allergies

--- components/views/userDetails.vue
+++ components/views/userDetails.vue
@@ -1,5 +1,5 @@
-<script setup lang="ts" >
-import { defineProps } from 'vue';
+<script setup lang="ts">
+import { defineProps } from 'vue'
 
 const prop = defineProps({
   userData: Object
@@ -8,41 +8,56 @@ const prop = defineProps({
 </script>
 
 <template>
-      <div class="my-5 bg-white border border-1 border-gray-100
-       shadow-sm flex-2">
-        <h1 class="bg-orange-400 p-5 flex items-center text-lg"><UIcon name="i-heroicons-user-20-solid" class="mr-3"/> Patient Details</h1>
-        <div class="px-5 py-2">
-          <b>Names: </b>
-          <span>{{prop.userData.firstName}} {{prop.userData.middleName}} {{prop.userData.lastName}}</span>
-        </div>
-        <div class="px-5 py-2">
-          <b>Age: </b>
-          <p>{{prop.userData.age}}</p>
-        </div>
+  <div
+    class="my-5 bg-white border border-1 border-gray-100
+       shadow-sm flex-2"
+  >
+    <h1 class="bg-orange-400 p-5 flex items-center text-lg">
+      <UIcon name="i-heroicons-user-20-solid" class="mr-3" /> Patient Details
+    </h1>
+    <div class="px-5 py-2 flex justify-between bg-gray-100 m-3">
+      <b class="flex-1">Names: </b>
+      <span  class="flex-1">{{ prop.userData.firstName }} {{ prop.userData.middleName }} {{ prop.userData.lastName }}</span>
+    </div>
+    <div class="px-5 py-2 flex justify-between bg-gray-100 m-3">
+      <b class="flex-1">Age: </b>
+      <span  class="flex-1">{{ prop.userData.age }}</span>
+    </div>
 
-        <div class="px-5 py-2">
-          <b>Height: </b>
-          <p>{{prop.userData.height}}</p>
-        </div>
-        <div class="px-5 py-2">
-          <b>Weight: </b>
-          <p>{{prop.userData.weight}}</p>
-        </div>
-        <div class="px-5 py-2">
-          <b>Email: </b>
-          <p>{{prop.userData.email}}</p>
-        </div>
+    <div class="px-5 py-2 flex justify-between bg-gray-100 m-3">
+      <b class="flex-1">Height: </b>
+      <p  class="flex-1">{{ prop.userData.height }}</p>
+    </div>
+    <div class="px-5 py-2 flex justify-between bg-gray-100 m-3">
+      <b class="flex-1">Weight: </b>
+      <p class="flex-1">{{ prop.userData.weight }}</p>
+    </div>
+    <div class="px-5 py-2 flex justify-between bg-gray-100 m-3">
+      <b class="flex-1">Email: </b>
+      <p class="flex-1">{{ prop.userData.email }}</p>
+    </div>
 
-        <div class="px-5 py-2">
-          <b>Contact Number: </b>
-          <p>+254{{prop.userData.contactNumber}}</p>
-        </div>
-        <div class="px-5 py-2">
-          <b>Address: </b>
-          <p>{{prop.userData.address}}</p>
-        </div>
+    <div class="px-5 py-2 flex justify-between bg-gray-100 m-3">
+      <b class="flex-1">Contact Number: </b>
+      <p class="flex-1">+254{{ prop.userData.contactNumber }}</p>
+    </div>
+    <div class="px-5 py-2 flex justify-between bg-gray-100 m-3">
+      <b class="flex-1">Address: </b>
+      <p class="flex-1">{{ prop.userData.address }}</p>
+    </div>
+    <div class="px-5 py-2 flex justify-between bg-gray-100 m-3">
+      <b class="flex-1">Allergies: </b>
+      <div class="flex-1">
+        <p>Meat</p>
+        <p>Dairy</p>
+        <p>Pets</p>
       </div>
-
+    </div>
+    <div class="px-5 py-2 flex justify-between bg-gray-100 m-3">
+      <b class="flex-1">Date Registered: </b>
+      <p class="flex-1">{{ prop.userData.createdAt }}</p>
+    </div>
+  </div>
 </template>
 
 <style scoped>
